cd6fac1e5a054d5a6393b48c0c35f0e9b73efe7c
Vinko Kolobara <kolobara.vinko@gmail.com>
2017-12-05 19:45:03 +0100

    Slow day5 done

--- Day5/Day5.hs
+++ Day5/Day5.hs
@@ -7,13 +7,26 @@ import Lib.IOLib
 input :: [Int]
 input = [0,3,0,1,-3]
 
-jump xs i acc
+jump1 xs i acc
   | i < 0 || i >= len = acc
-  | otherwise         = jump newL (i + (xs !! i)) acc + 1
+  | otherwise         = jump1 newL (i + curr) acc + 1
   where len  = length xs
-        newL = take i xs ++ [(xs !! i) + 1] ++ drop (i+1) xs
+        newL = take i xs ++ [curr + 1] ++ drop (i+1) xs
+        curr = xs !! i
+
+jump2 xs i acc
+  | i < 0 || i >= len = acc
+  | otherwise         = jump2 newL (i + curr) acc + 1
+  where len  = length xs
+        newL = take i xs ++ [curr + inc] ++ drop (i+1) xs
+        inc  = if curr >= 3 then -1 else 1 
+        curr = xs !! i
+
+part1 = jump1
+part2 = jump2
 
 main = do
   lines <- readLinesToStringList
   let inp = map (read :: String->Int) lines
-  print $ jump inp 0 0
+--  print $ part1 inp 0 0
+  print $ part2 inp 0 0
