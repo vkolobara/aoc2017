5c42e59cf1387c525af416b09ec90287d08f78c3
Vinko Kolobara <kolobara.vinko@gmail.com>
2017-12-05 19:52:14 +0100

    Faster implementation of day5 with Data.Sequence

--- Day5/Day5.hs
+++ Day5/Day5.hs
@@ -1,5 +1,6 @@
 import Data.Char
 import Data.List
+import Data.Sequence as Seq
 import System.Environment
 import System.IO
 import Lib.IOLib
@@ -10,23 +11,23 @@ input = [0,3,0,1,-3]
 jump1 xs i acc
   | i < 0 || i >= len = acc
   | otherwise         = jump1 newL (i + curr) acc + 1
-  where len  = length xs
-        newL = take i xs ++ [curr + 1] ++ drop (i+1) xs
-        curr = xs !! i
+  where len  = Seq.length xs
+        newL = Seq.update i (curr+1) xs
+        curr = index xs i
 
 jump2 xs i acc
   | i < 0 || i >= len = acc
   | otherwise         = jump2 newL (i + curr) acc + 1
-  where len  = length xs
-        newL = take i xs ++ [curr + inc] ++ drop (i+1) xs
+  where len  = Seq.length xs
+        newL = Seq.update i (curr+inc) xs
         inc  = if curr >= 3 then -1 else 1 
-        curr = xs !! i
+        curr = index xs i
 
 part1 = jump1
 part2 = jump2
 
 main = do
   lines <- readLinesToStringList
-  let inp = map (read :: String->Int) lines
---  print $ part1 inp 0 0
+  let inp = (Seq.fromList . map (read :: String->Int)) lines
+  print $ part1 inp 0 0
   print $ part2 inp 0 0
